06c026fc4cfe70fb94faf7be412c6d09463eb565
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-05-27 01:08:19 +0900

    caregoryモデルの追加

---
+++ app/models/category.rb
@@ -0,0 +1,12 @@
+class Category < ApplicationRecord
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '建築・土木' },
+    { id: 3, name: '製品設計・製造' },
+    { id: 4, name: '映画・VFX' },
+    { id: 5, name: 'ゲーム・VR' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
