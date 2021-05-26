5a853db2e69553daf8d5290a50f3060f7304160c
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-05-27 01:23:49 +0900

    ActiveHashに関する不要なファイルの削除

--- db/migrate/20210526161006_create_softwares.rb
+++
@@ -1,8 +0,0 @@
-class CreateSoftwares < ActiveRecord::Migration[6.0]
-  def change
-    create_table :softwares do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20210526161328_create_os.rb
+++
@@ -1,8 +0,0 @@
-class CreateOs < ActiveRecord::Migration[6.0]
-  def change
-    create_table :os do |t|
-
-      t.timestamps
-    end
-  end
-end
