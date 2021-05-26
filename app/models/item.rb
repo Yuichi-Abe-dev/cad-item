59856f7882ea13c476761abb7106c0fdfdf485b8
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-05-27 01:37:21 +0900

    Itemモデルにアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :category
+  belongs_to :software
+  belongs_to :operating_system
+  belongs_to :status
+  belongs_to :user
   has_one_attached :image
+  with_options presence: true do
+    validates :image, :name, :description
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :software_id, :operating_system_id, :status_id
+    end
+  end
 end
