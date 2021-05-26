cabe6c4215e7d22c9203750e317fb0b1f2ce7321
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-05-27 05:40:32 +0900

    itemコントローラーにdestroyアクションの雛形の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+  end 
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :software_id, :version, :operating_system_id, :status_id, :image).merge(user_id: current_user.id)
