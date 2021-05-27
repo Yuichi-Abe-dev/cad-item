43c7937bbc86af4b841529b6d7a81752ab9acd23
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-05-27 10:58:31 +0900

    コントローラに検索用のメソッドを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,14 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_toppage, only: [:edit, :update, :destroy]
+  before_action :search_item, only: [:index, :search]
 
   def index
     @item = Item.includes(:user)
+  end
+
+  def search
+    @results = @p.result.includes(:category)  # 検索条件にマッチした商品の情報を取得
   end
 
   def new
@@ -54,5 +59,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def search_item
+    @p = Item.ransack(params[:q])  # 検索オブジェクトを生成
   end
 end
